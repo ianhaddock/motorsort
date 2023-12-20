0f942e7561a4d47889ad866e471dee8c5bdaaa67
Ian Haddock <ihaddock@mac.com>
2023-12-19 18:30:54 -0800

    sprint logic fixes

--- racefiles.py
+++ racefiles.py
@@ -17,7 +17,6 @@ def listfiles():
 
 def parsefilename(sourcefilename):
     teds = False
-
     for i, c in enumerate(sourcefilename):
 #        print(sourcefilename[i-6:i-2])
         if c in test_chars:
@@ -42,8 +41,8 @@ def parsefilename(sourcefilename):
                     raceepisode = '06'
                 elif racesession == 'FP3':
                     raceepisode = '11'
-            if sourcefilename[i:i+10] == 'Qualifying':
-                racesession = sourcefilename[i:i+10]
+            if sourcefilename[i:i+5] == 'Quali':
+                racesession = 'Qualifying' # sourcefilename[i:i+10]
                 racename = sourcefilename[race_name_index_start:i-1]
                 raceinfo = sourcefilename[i+11:-4]
                 raceepisode = '03'
@@ -52,24 +51,30 @@ def parsefilename(sourcefilename):
             if sourcefilename[i:i+4] == 'Race' or sourcefilename[i:i+4] == 'RACE':
                 if sourcefilename[i-5:i-1] == 'Teds':
                     racename = sourcefilename[race_name_index_start:i-6]
+                    racesession = 'Teds Notebook'
+                    raceepisode = '15'
                 elif sourcefilename[i-7:i-1] == 'Sprint':
                     racename = sourcefilename[race_name_index_start:i-8]
                     racesession = sourcefilename[i-7:i-1]
+                    raceepisode = '08'
                 else:
                     racename = sourcefilename[race_name_index_start:i-1]
-                racesession = sourcefilename[i:i+4]
+                    racesession = sourcefilename[i:i+4]
+                    raceepisode = '13'
                 raceinfo = sourcefilename[i+5:-4]
-                raceepisode = '13'
-            if sourcefilename[i:i+15] == 'Sprint Shootout':
-                racesession = sourcefilename[i:i+15]
-                racename = sourcefilename[race_name_index_start:i-1]
-                raceinfo = sourcefilename[i+16:-4]
-                raceepisode = '07'
             if sourcefilename[i:i+6] == 'Sprint':
-                racesession = sourcefilename[i:i+6]
-                racename = sourcefilename[race_name_index_start:i-1]
-                raceinfo = sourcefilename[i+7:-4]
-                raceepisode = '08'
+            #    print(sourcefilename[i:i+6])
+            #    print(sourcefilename[i+7:i+10])
+                if sourcefilename[i+7:i+10] == 'Sky':
+                    racesession = sourcefilename[i:i+6]
+                    racename = sourcefilename[race_name_index_start:i-1]
+                    raceinfo = sourcefilename[i+7:-4]
+                    raceepisode = '08'
+                if sourcefilename[i+7:i+10] == 'Sho':
+                    racesession = 'Sprint Shootout' #sourcefilename[i:i+15]
+                    racename = sourcefilename[race_name_index_start:i-1]
+                    raceinfo = sourcefilename[i+16:-4]
+                    raceepisode = '07'
 
     if teds is True:
         if racesession == 'Qualifying':
@@ -82,8 +87,8 @@ def parsefilename(sourcefilename):
             raceepisode = '15'
             racesession = 'Race Notebook'
 
-
     return (race_series, raceseason, raceround, racename, raceepisode, racesession, raceinfo)
+
 
 sourcefilenames = listfiles()
 
@@ -93,23 +98,27 @@ for sourcefilename in sourcefilenames:
 
     sourcefilename_clean = sourcefilename.replace('.', ' ')
 
-    # file checks 
     if sourcefilename[-3:] in file_types:
         filetype = sourcefilename[-3:]
 
         race_series, raceseason, raceround, racename, raceepisode, racesession, raceinfo = parsefilename(sourcefilename_clean)
 
         foldername = str(raceseason + "-" + raceround + " - " + racename + " GP")
+
         finalfilename = str(racename + " GP - S" + raceround + "E" + raceepisode + " - " + racesession + " [" + raceinfo + "]." + filetype)
+
         destfolder = str("mediafiles/" + race_series + "/" + foldername)
-        print(destfolder)
+
+        print(destfolder + "/" + finalfilename)
         os.makedirs(destfolder, exist_ok=True)
-        print("Parsed filename: " + finalfilename)
+
+
         sourcefilepath = str('../sourcefiles/' + sourcefilename)
         finalfilepath = str(destfolder + '/' + finalfilename)
+
         try:
             os.symlink(sourcefilepath, finalfilepath)
         except FileExistsError as err:
-            print("Symlink exists: " + str(err))
+            print("Symlink exists: ") #  + str(err))
 
 
