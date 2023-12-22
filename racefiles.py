7a08cbe83d56374178570a0468d1cd1391950fb0
Ian Haddock <ihaddock@mac.com>
2023-12-22 14:04:52 -0800

    readability

--- racefiles.py
+++ racefiles.py
@@ -4,7 +4,8 @@
 import os
 
 file_types = ['mkv', 'mp4']
-source_files = 'sourcefiles'
+source_path = 'sourcefiles'
+destination_path = 'mediafiles'
 
 sprint_weekends = [('2024', '00')]
 
@@ -39,96 +40,96 @@ regular_order = ['Free Practice 1',
                  'Onboard Channel']
 
 
-def listfiles(source_files):
-    filelist = os.listdir(source_files)
+def list_files(source_path):
+    filelist = os.listdir(source_path)
     return filelist
 
 
-def parsefilename(sourcefilename):
-    """ parse source file names for keywords based on test_cars list"""
+def parse_file_name(source_file_name):
+    """ parse source file names for keywords based on test_chars list"""
 
     test_chars = ['2', 'A', 'T', 'F', 'O', 'Q', 'R', 'S', 'W']
     teds = False
-    race_name = sourcefilename
+    race_name = source_file_name
 
-    for i, c in enumerate(sourcefilename):
-        # print(sourcefilename[i-6:i-2])
+    for i, c in enumerate(source_file_name):
+        # print(source_file_name[i-6:i-2])
         if c in test_chars:
-            if sourcefilename[i:i+3] == 'WEC':
+            if source_file_name[i:i+3] == 'WEC':
                 race_series = 'WEC'
-            if sourcefilename[i:i+8] == 'Formula1' or \
-               sourcefilename[i:i+9] == 'Formula 1':
+            if source_file_name[i:i+8] == 'Formula1' or \
+               source_file_name[i:i+9] == 'Formula 1':
                 race_series = 'Formula 1'
-            if sourcefilename[i:i+4].isnumeric():
-                race_season = sourcefilename[i:i+4]
-            if sourcefilename[i:i+5] == 'Round':
-                race_round = sourcefilename[i+5:i+7]
+            if source_file_name[i:i+4].isnumeric():
+                race_season = source_file_name[i:i+4]
+            if source_file_name[i:i+5] == 'Round':
+                race_round = source_file_name[i+5:i+7]
                 race_name_index_start = i+8
-            if sourcefilename[i:i+4] == 'Teds':
+            if source_file_name[i:i+4] == 'Teds':
                 teds = True
-                teds_race_name = sourcefilename[race_name_index_start:i-1]
+                teds_race_name = source_file_name[race_name_index_start:i-1]
 
-            # print(sourcefilename[i:i+7])
-            if sourcefilename[i:i+7].lower() == 'onboard':
+            # print(source_file_name[i:i+7])
+            if source_file_name[i:i+7].lower() == 'onboard':
                 race_session = 'Onboard Channel'
-                race_name = sourcefilename[race_name_index_start:i-1]
-                race_info = sourcefilename[i+16:-4]
+                race_name = source_file_name[race_name_index_start:i-1]
+                race_info = source_file_name[i+16:-4]
 
-            # print(sourcefilename[i:i+2])
-            if sourcefilename[i:i+2].lower() == 'fp':
-                race_session = str("Free Practice " + sourcefilename[i+2])
-                race_name = sourcefilename[race_name_index_start:i-1]
-                race_info = sourcefilename[i+4:-4]
+            # print(source_file_name[i:i+2])
+            if source_file_name[i:i+2].lower() == 'fp':
+                race_session = str("Free Practice " + source_file_name[i+2])
+                race_name = source_file_name[race_name_index_start:i-1]
+                race_info = source_file_name[i+4:-4]
 
-            # print(sourcefilename[i:i+10])
-            if sourcefilename[i:i+11].lower() == 'qualifying ':
-                race_name = sourcefilename[race_name_index_start:i-1]
-                if sourcefilename[i:i+19] == 'Qualifying Analysis':
+            # print(source_file_name[i:i+10])
+            if source_file_name[i:i+11].lower() == 'qualifying ':
+                race_name = source_file_name[race_name_index_start:i-1]
+                if source_file_name[i:i+19] == 'Qualifying Analysis':
                     race_session = 'Quali Analysis'
-                    race_info = sourcefilename[i+20:-4]
-                elif sourcefilename[i:i+18] == 'Qualifying Buildup':
+                    race_info = source_file_name[i+20:-4]
+                elif source_file_name[i:i+18] == 'Qualifying Buildup':
                     race_session = 'Quali Buildup'
-                    race_info = sourcefilename[i+19:-4]
+                    race_info = source_file_name[i+19:-4]
                 else:
                     race_session = 'Qualifying'
-                    race_info = sourcefilename[i+11:-4]
+                    race_info = source_file_name[i+11:-4]
 
-            # print(sourcefilename[i:i+6])
-            if sourcefilename[i:i+6].lower() == 'quali ':
-                race_name = sourcefilename[race_name_index_start:i-1]
-                if sourcefilename[i:i+14] == 'Quali Analysis':
+            # print(source_file_name[i:i+6])
+            if source_file_name[i:i+6].lower() == 'quali ':
+                race_name = source_file_name[race_name_index_start:i-1]
+                if source_file_name[i:i+14] == 'Quali Analysis':
                     race_session = 'Quali Analysis'
-                    race_info = sourcefilename[i+15:-4]
-                elif sourcefilename[i:i+13] == 'Quali Buildup':
+                    race_info = source_file_name[i+15:-4]
+                elif source_file_name[i:i+13] == 'Quali Buildup':
                     race_session = 'Quali Buildup'
-                    race_info = sourcefilename[i+14:-4]
+                    race_info = source_file_name[i+14:-4]
                 else:
                     race_session = 'Qualifying'
-                    race_info = sourcefilename[i+6:-4]
+                    race_info = source_file_name[i+6:-4]
 
-            #print(sourcefilename[i-7:i-1])
-            if sourcefilename[i:i+5].lower() == 'race ':
-                if len(race_name) > len(sourcefilename[race_name_index_start:i-1]):
-                    race_name = sourcefilename[race_name_index_start:i-1]
-                if sourcefilename[i:i+13] == 'Race Analysis':
+            # print(source_file_name[i-7:i-1])
+            if source_file_name[i:i+5].lower() == 'race ':
+                if len(race_name) > len(source_file_name[race_name_index_start:i-1]):
+                    race_name = source_file_name[race_name_index_start:i-1]
+                if source_file_name[i:i+13] == 'Race Analysis':
                     race_session = 'Race Analysis'
-                    race_info = sourcefilename[i+14:-4]
-                elif sourcefilename[i:i+12] == 'Race Buildup':
+                    race_info = source_file_name[i+14:-4]
+                elif source_file_name[i:i+12] == 'Race Buildup':
                     race_session = 'Race Buildup'
-                    race_info = sourcefilename[i+13:-4]
+                    race_info = source_file_name[i+13:-4]
                 else:
                     race_session = 'Race'
-                    race_info = sourcefilename[i+5:-4]
+                    race_info = source_file_name[i+5:-4]
 
-            # print(sourcefilename[i:i+6])
-            if sourcefilename[i:i+7].lower() == 'sprint ':
-                race_name = sourcefilename[race_name_index_start:i-1]
-                if sourcefilename[i:i+10] == 'Sprint Sho':
+            # print(source_file_name[i:i+6])
+            if source_file_name[i:i+7].lower() == 'sprint ':
+                race_name = source_file_name[race_name_index_start:i-1]
+                if source_file_name[i:i+10] == 'Sprint Sho':
                     race_session = 'Sprint Shootout'
-                    race_info = sourcefilename[i+16:-4]
+                    race_info = source_file_name[i+16:-4]
                 else:
                     race_session = 'Sprint'
-                    race_info = sourcefilename[i+7:-4]
+                    race_info = source_file_name[i+7:-4]
 
     if teds is True:
         if len(teds_race_name) < len(race_name):
@@ -144,85 +145,74 @@ def parsefilename(sourcefilename):
             race_info)
 
 
-
-
-def find_sprint_weekends(sourcefilenames):
-    # search for sprint weekends before parsing names
-    for sourcefilename in sourcefilenames:
-        for i, c in enumerate(sourcefilename):
+def find_sprint_weekends(source_file_names, sprint_weekends):
+    """ search for sprint weekends before parsing names """
+    for source_file_name in source_file_names:
+        for i, c in enumerate(source_file_name):
             if c == '2':
-                if sourcefilename[i:i+4].isnumeric():
-                    race_season = sourcefilename[i:i+4]
-                # print(sourcefilename[i:i+4])
+                if source_file_name[i:i+4].isnumeric():
+                    race_season = source_file_name[i:i+4]
+                # print(source_file_name[i:i+4])
             if c == 'R':
-                # print(sourcefilename[i:i+5])
-                if sourcefilename[i:i+5] == 'Round':
-                    race_round = sourcefilename[i+5:i+7]
-                    # print(sourcefilename[i+5:i+7])
+                # print(source_file_name[i:i+5])
+                if source_file_name[i:i+5] == 'Round':
+                    race_round = source_file_name[i+5:i+7]
+                    # print(source_file_name[i+5:i+7])
             if c == 'S':
-                # print(sourcefilename[i:i+6])
-                if sourcefilename[i:i+6] == 'Sprint':
+                # print(source_file_name[i:i+6])
+                if source_file_name[i:i+6] == 'Sprint':
                     if (race_season, race_round) not in sprint_weekends:
                         sprint_weekends.append((race_season, race_round))
     return sprint_weekends
 
 
-def sort_files(sourcefilenames):
-    for sourcefilename in sourcefilenames:
+def build_out_files(source_file_names, sprint_weekends):
+    """ check if this is a media file, parse filename, sort by keywords into
+    folders, link files to destination directory"""
+
+    for source_file_name in source_file_names:
         print()
-        print("Source filename: " + sourcefilename)
+        print(source_file_name)
 
-        if sourcefilename[-3:] in file_types:
+        if source_file_name[-3:] in file_types:
 
-            sourcefilename_clean = sourcefilename.replace('.', ' ')
+            filetype = source_file_name[-3:]
 
-            filetype = sourcefilename[-3:]
-
-            race_series, race_season, race_round, race_name, race_session, race_info = parsefilename(sourcefilename_clean)
+            race_series, race_season, race_round, race_name, race_session, \
+                race_info = parse_file_name(source_file_name.replace('.', ' '))
 
             if (race_season, race_round) in sprint_weekends:
                 weekend_order = str(sprint_order.index(race_session)+1).zfill(2)
             else:
                 weekend_order = str(regular_order.index(race_session)+1).zfill(2)
 
-            finalfilename = str(race_name + " GP - S" + race_round + "E" + weekend_order + " - " + race_session + " [" + race_info + "]." + filetype)
+            final_file_name = str(race_name + " GP - S" + race_round + "E" +
+                                  weekend_order + " - " + race_session +
+                                  " [" + race_info + "]." + filetype)
 
-            foldername = str(race_season + "-" + race_round + " - " + race_name + " GP")
-            destfolder = str("mediafiles/" + race_series + "/" + foldername)
+            destination_folder = str(destination_path + "/" + race_series +
+                                     "/" + race_season + "-" + race_round +
+                                     " - " + race_name + " GP")
 
-            print(destfolder + "/" + finalfilename)
-            os.makedirs(destfolder, exist_ok=True)
+            # print(destination_folder + "/" + final_file_name)
+            os.makedirs(destination_folder, exist_ok=True)
 
-            sourcefilepath = str(source_files + "/" + sourcefilename)
-            finalfilepath = str(destfolder + '/' + finalfilename)
+            source_file_path = str(source_path + "/" + source_file_name)
+            final_file_path = str(destination_folder + '/' + final_file_name)
 
             try:
-                os.link(sourcefilepath, finalfilepath)
+                print(final_file_path)
+                # os.link(source_file_path, final_file_path)
             except FileExistsError as err:
-                print("file exists: ") #  + str(err))
+                print(str(err))
 
 
-sourcefilenames = listfiles(source_files)
+if __name__ == "__main__":
+    source_file_names = list_files(source_path)
 
-print(find_sprint_weekends(sourcefilenames))
+    sprint_weekends = find_sprint_weekends(source_file_names, sprint_weekends)
 
-sort_files(sourcefilenames)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    build_out_files(source_file_names, sprint_weekends)
 
 
 
