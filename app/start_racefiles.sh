d9f3a9578fadd2d6b9f88db712a663cebb7b9a29
Ian Haddock <ihaddock@mac.com>
2024-06-08 09:39:57 -0700

    start_racefiles.sh: run once and exit when SLEEP_SECONDS env var set to zero

--- app/start_racefiles.sh
+++ app/start_racefiles.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
+# if env var set use that, otherwise use default
+#
 if [ ! $SLEEP_SECONDS ]; then
     sleep_seconds=300
 else
     sleep_seconds=$SLEEP_SECONDS
 fi
 
+# take seconds and divide by ten for progress indicator
+# if value is zero seconds, use 1 second instead
+#
 tic_rate=$(echo $sleep_seconds / 10 | bc)
-
 if [ $tic_rate -lt 1 ]; then
     tic_rate=1
 fi
 
+# if env var is zero, run then quit. Otherwise loop 
+# every $sleep_seconds forever
+#
 while true; do
-    echo "$(date): starting"
-#    python racefiles.py
-    echo "$(date): sleeping $sleep_seconds seconds"
+    echo "$(date): Starting"
+    python racefiles.py
+
+    if [ $sleep_seconds == 0 ]; then
+        echo "$(date): Exiting."
+        exit 0
+    fi
+
+    echo "$(date): Sleeping $sleep_seconds seconds"
 
     n=0
     while (( n < $sleep_seconds )); do
