ba44d761886ca99b744ba4ae478c43b40e298854
Ian Haddock <ihaddock@mac.com>
2024-12-25 15:12:28 -0800

    pytest: first pass for unit testing

---
+++ conftest.py
@@ -0,0 +1,8 @@
+# conftest.py
+
+import os
+
+os.environ['MEDIA_SOURCE_PATH'] = "media/source_files/complete"
+os.environ['MEDIA_DESTINATION_PATH'] = "media/"
+os.environ['COPY_FILES'] = "False"
+os.environ['CONFIG_PATH'] = "config"
