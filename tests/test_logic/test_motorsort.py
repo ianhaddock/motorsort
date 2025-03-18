30a23e0815ad3c30bad1e73a87eb90e4beda7822
Ian Haddock <ianhaddock@users.noreply.github.com>
2025-03-18 09:49:37 -0700

    Config update (#6)
    * motorsort: use regex to parse file names
    start_motorsort: copy json files to custom directory at start
    config.ini: create json group
    
    * pytest: update tests
    
    * config.ini: break out paths to group

--- tests/test_logic/test_motorsort.py
+++ tests/test_logic/test_motorsort.py
@@ -5,34 +5,19 @@ import os
 import shutil
 import json
 from configparser import ConfigParser
-from motorsort import main, weekend, parse_file_name, get_file_list, find_sprint_weekends
+from motorsort import main, parse_file_name, get_file_list, find_sprint_weekends
+from weekend import Weekend
 
-sprint_weekends = [(2024, '05'), ('2023', '21')]
-destination_path = "destination_top"
-
-race = weekend()
-race.set_race_name('race_name')
-race.set_gp_suffix('')
-race.set_race_round('01')
-race.set_weekend_order('02')
-race.set_race_session('Qualifying')
-race.set_race_info('Race Info')
-race.set_filetype('mov')
-race.set_race_series("Race Series")
-race.set_race_season("2024")
-
-# read config.ini file
 config = ConfigParser()
 config.read('config/config.ini')
 file_prefix = tuple(config.get('config', 'file_prefix').split(','))
 file_types = tuple(config.get('config', 'file_types').split(','))
 weekends = config.get('config', 'sprint_weekends').split(',')
-image_path = config.get('config', 'image_path')
-track_path = config.get('config', 'track_path')
-flag_path = config.get('config', 'flag_path')
-font_path = config.get('config', 'font_path')
+image_path = config.get('paths', 'image_path')
+track_path = config.get('paths', 'track_path')
+flag_path = config.get('paths', 'flag_path')
+font_path = config.get('paths', 'font_path')
 
-# read json files
 with open('config/series_prefix.json') as file:
     series_prefix = json.load(file)
 with open('config/weekend_order.json') as file:
@@ -43,50 +28,66 @@ with open('config/fonts.json') as file:
     font_list = json.load(file)
 
 
-def test_get_final_name():
+def test_get_final_name(tmp_path):
 
-    assert race.get_race_info() == "Race Info"
-    assert race.get_race_season() == "2024"
+    race = Weekend(tmp_path)
+    race.set_race_name('race_name')
+    race.set_race_round('01')
+    race.set_weekend_order('02')
+    race.set_race_session('Qualifying')
+    race.set_race_info('Race Info')
+    race.set_filetype('.mov')
+
     assert race.get_final_file_name() == 'race_name - S01E02 - Qualifying [Race Info].mov'
 
 
 def test_get_destination_folder_new_path(tmp_path):
 
-    destination_path = f'{tmp_path}/motorsort'
-    os.makedirs(destination_path)
+    race = Weekend(f'{tmp_path}/motorsort')
+    race.set_race_series("Race Series")
+    race.set_race_name('race_name')
+    race.set_race_season("2024")
+    race.set_race_round('01')
 
-    assert race.get_destination_folder(destination_path) == f'{tmp_path}/motorsort/Race Series/2024-01 - race_name '
+    assert race.get_destination_folder() == f'{tmp_path}/motorsort/Race Series/2024-01 - race_name'
 
 
 def test_get_destination_folder_partial_path_match(tmp_path):
 
-    existing_destination_path = f'{tmp_path}/motorsort/Race Series/2024-01 - other_race_name'
-    os.makedirs(existing_destination_path)
-    requested_destination_path = f'{tmp_path}/motorsort'
+    race = Weekend(f'{tmp_path}/motorsort')
+    race.set_race_series("Race Series")
+    race.set_race_name('race_name')
+    race.set_race_season("2024")
+    race.set_race_round('01')
+    os.makedirs(f'{tmp_path}/motorsort/Race Series/2024-01 - other_race_name')
 
-    assert race.get_destination_folder(requested_destination_path) == f'{tmp_path}/motorsort/Race Series/2024-01 - other_race_name'
+    assert race.get_destination_folder() == f'{tmp_path}/motorsort/Race Series/2024-01 - other_race_name'
 
 
-def test_parse_file_name_formula_1_regular_weekend():
+def test_parse_file_name_formula_1_regular_weekend(tmp_path):
 
-    race.set_gp_suffix('')
+    race = Weekend(f'{tmp_path}/motorsort')
+    sprint_weekends = [(2024, '05'), ('2023', '21')]
     file_name = 'test_media/Formula1.2022.Round04.Example.FP1.FastChannelHD.1080p.50fps.X264.Multi-AOA11 mkv'
     parse_file_name(race, series_prefix, session_map, sprint_weekends, the_weekend_order, file_name)
 
-    assert race.get_race_series() == 'Formula 1' and race.get_gp_suffix() == ' GP'
+    assert race.get_final_file_name() == 'Example GP - S04E01 - Free Practice 1 [FastChannelHD 1080p 50fps X264].Multi-AOA11 mkv'
 
 
-def test_parse_file_name_formula_1_sprint_weekend():
+def test_parse_file_name_formula_1_sprint_weekend(tmp_path):
 
+    race = Weekend(f'{tmp_path}/motorsort')
+    sprint_weekends = [(2024, '05'), ('2023', '21')]
     file_name = 'test_media/Formula1.2023.Round21.Example.Sprint.FastChannelHD.1080p.50fps.X264.Multi-AOA11.mkv'
     parse_file_name(race, series_prefix, session_map, sprint_weekends, the_weekend_order, file_name)
 
     assert race.get_race_session() == 'Sprint'
 
 
-def test_parse_file_name_wec_and_no_suffix():
+def test_parse_file_name_wec_and_no_suffix(tmp_path):
 
-    race.set_gp_suffix(' GP')
+    race = Weekend(f'{tmp_path}/motorsort')
+    sprint_weekends = [(2024, '05'), ('2023', '21')]
     file_name = 'test_media/WEC.2022.Round04.Example.Race04.FastChannelHD.1080p.50fps.X264.Multi-AOA11.mkv'
     parse_file_name(race, series_prefix, session_map, sprint_weekends, the_weekend_order, file_name)
 
@@ -94,36 +95,44 @@ def test_parse_file_name_wec_and_no_suffix():
     assert not race.get_gp_suffix() == ' GP'
 
 
-def test_parse_file_name_no_round_name():
+def test_parse_file_name_no_round_name(tmp_path):
 
+    race = Weekend(f'{tmp_path}/motorsort')
+    sprint_weekends = [(2024, '05'), ('2023', '21')]
     file_name = 'test_media/Formula1.2022.Round03.FP1.FastChannelHD.1080p.50fps.X264.Multi-AOA11.mkv'
     parse_file_name(race, series_prefix, session_map, sprint_weekends, the_weekend_order, file_name)
 
     assert race.get_race_name() == ''
 
 
-def test_parse_file_name_no_round_number():
+def test_parse_file_name_no_round_number(tmp_path):
 
+    race = Weekend(f'{tmp_path}/motorsort')
+    sprint_weekends = [(2024, '05'), ('2023', '21')]
     file_name = 'test_media/Formula1.2022.Example.FP1.FastChannelHD.1080p.50fps.X264.Multi-AOA11.mkv'
     parse_file_name(race, series_prefix, session_map, sprint_weekends, the_weekend_order, file_name)
 
     assert race.get_race_round() == '00'
 
 
-def test_parse_file_usa_remove():
+def test_parse_file_usa_remove(tmp_path):
 
+    race = Weekend(f'{tmp_path}/motorsort')
+    sprint_weekends = [(2024, '05'), ('2023', '21')]
     file_name = 'test_media/Formula1.2022.Round04.USA.Example.FP1.FastChannelHD.1080p.50fps.X264.Multi-AOA11.mkv'
     parse_file_name(race, series_prefix, session_map, sprint_weekends, the_weekend_order, file_name)
 
-    assert not race.get_race_name() == 'USA Example'
+    assert race.get_race_name() == 'Example'
 
 
-def test_parse_file_france_remove():
+def test_parse_file_france_remove(tmp_path):
 
+    race = Weekend(f'{tmp_path}/motorsort')
+    sprint_weekends = [(2024, '05'), ('2023', '21')]
     file_name = 'test_media/WEC.2022.Round04.France.Example.FP1.FastChannelHD.1080p.50fps.X264.Multi-AOA11.mkv'
     parse_file_name(race, series_prefix, session_map, sprint_weekends, the_weekend_order, file_name)
 
-    assert not race.get_race_name() == 'France Example'
+    assert race.get_race_name() == 'Example'
 
 
 def test_get_file_list_source_path_error():
@@ -181,32 +190,11 @@ def test_find_sprint_weekends(tmp_path):
 
     assert find_sprint_weekends(source_file_names, weekends) == \
         [
-           ('2025', '05'),
-           ('2025', '06'),
-           ('2025', '11'),
-           ('2025', '19'),
-           ('2025', '21'),
-           ('2025', '23'),
-           ('2022', '00')
+        ('2025', '05'),
+        ('2025', '06'),
+        ('2025', '11'),
+        ('2025', '19'),
+        ('2025', '21'),
+        ('2025', '23'),
+        ('2022', '00')
         ]
-
-
-def test_main_link_files(tmp_path):
-    os.environ['MEDIA_DESTINATION_PATH'] = f"{tmp_path}/media/"
-    os.environ['CONFIG_PATH'] = 'config'
-
-    # test file exist path
-    destination_path = f'{tmp_path}/media/Formula 1/2022-00 - Example GP'
-    os.makedirs(destination_path)
-    shutil.copy('tests/test_data/Example GP - S00E10 - Sprint Notebook [FastChannelHD 1080p 50fps X264 Multi-AOA11].mkv', f'{tmp_path}/media/Formula 1/2022-00 - Example GP/Example GP - S00E10 - Sprint Notebook [FastChannelHD 1080p 50fps X264 Multi-AOA11].mkv')
-
-    assert main() == 0
-
-
-def test_main_copy_files(tmp_path):
-    os.environ['MEDIA_DESTINATION_PATH'] = f"{tmp_path}/media/"
-    os.environ['CONFIG_PATH'] = 'config'
-    os.environ['COPY_FILES'] = 'True'
-
-    assert main() == 0
-
